ae9ad327ab4e634d11df9d2046460ae1bea5b3f7
tarrmmy <tarrmmy@gmail.com>
2023-10-31 22:00:22 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import {Button} from 'flowbite-react';
+import { useState } from 'react';
 // import { FloatingLabel } from 'flowbite-react';
 // import LinkComponent from ''
 // import './App.css';
 
 function App() {
+
+    const [payload, setPayload] = useState({
+        first_name : '',
+        last_name : '',
+        email : '',
+        phone_number: '',
+        gender : '',
+        age_range : '',
+        category : '',
+        school_name : '',
+        branch_id : '',
+        class : '',
+    })
     const handleRegister = (e) => {
         e.preventDefault()
         const success = document.getElementById("success_alert")
         const button = document.getElementById("submit_button")
         const error = document.getElementById("error_alert")
-        const payload = {
-            firstName: document.getElementById("first_name").value,
-            lastName: document.getElementById("last_name").value,
-            phoneNumber: document.getElementById("last_name").value,
-            branchId: document.getElementById("branch_id").value,
-            schoolName: document.getElementById("school_name").value,
-            class: document.getElementById("class").value,
-            gender: document.getElementById("gender").value,
-            // paymentMethod: document.getElementById("payment_method").value,
-            category: document.getElementById("category").value,
-            // stateOfOrigin: document.getElementById("state_of_origin").value,
-            email: document.getElementById("email").value,
-        }
+        // const payload = {
+        //     firstName: document.getElementById("first_name").value,
+        //     lastName: document.getElementById("last_name").value,
+        //     phoneNumber: document.getElementById("last_name").value,
+        //     branchId: document.getElementById("branch_id").value,
+        //     schoolName: document.getElementById("school_name").value,
+        //     class: document.getElementById("class").value,
+        //     gender: document.getElementById("gender").value,
+        //     paymentMethod: document.getElementById("payment_method").value,
+        //     category: document.getElementById("category").value,
+        //     stateOfOrigin: document.getElementById("state_of_origin").value,
+        //     email: document.getElementById("email").value,
+        // }
+
+        
 
         try{
             button.innerHTML = "Loading..."
@@ -58,31 +74,32 @@ function App() {
     <div className="App items-center">
         {/* <h1 className='text-primary'>Hello</h1> */}
         <div className='py-20'>
+            <h2 className='text-center mb-4'>Register for the MDC 2023 Major Outing</h2>
             <form
                 onSubmit={handleRegister}
                 className="flex max-w-md border mx-auto rounded-md flex-col p-10 gap-4">
                 <div className="relative mb-2">
-                    <input type="text" id="first_name" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input onChange={(e) => setPayload({...payload, first_name: e.target.value})} type="text" id="first_name" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">First Name</label>
                 </div>
                 <div className="relative mb-2">
-                    <input type="text" id="last_name" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input onChange={(e) => setPayload({...payload, last_name: e.target.value})} type="text" id="last_name" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">Last Name</label>
                 </div>
                 <div className="relative mb-2">
-                    <input type="text" id="email" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input onChange={(e) => setPayload({...payload, email: e.target.value})} type="text" id="email" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">Email</label>
                 </div>
                 <div className="relative mb-2">
-                    <input type="text" id="phone_number" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input onChange={(e) => setPayload({...payload, phone_number: e.target.value})} type="tel" id="phone_number" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">Phone Number (Whatsapp)</label>
                 </div>
-                <select id="gender" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select onChange={(e) => setPayload({...payload, gender: e.target.value})} id="gender" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>Gender</option>
                     <option>Male</option>
                     <option>Female</option>
                 </select>
-                <select id="" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select onChange={(e) => setPayload({...payload, age_range: e.target.value})} id="age_range" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>Age Range</option>
                     <option>10 and below</option>
                     <option>10 - 15</option>
@@ -91,7 +108,7 @@ function App() {
                     <option>25 - 30</option>
                     <option>31 and above</option>
                 </select>
-                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select onChange={(e) => setPayload({...payload, category: e.target.value})} id="category" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>Category</option>
                     <option >Primary school student</option>
                     <option>Junior secondary school student</option>
@@ -101,25 +118,25 @@ function App() {
                     <option>Adult</option>
                 </select>
                 <div className="relative mb-2">
-                    <input type="text" id="school" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input onChange={(e) => setPayload({...payload, school_name: e.target.value})} type="text" id="school_name" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">School</label>
                 </div>
-                <select id="branch_id" className="bg-gray-50 border border-gray-300 text-gray-900 mb-3 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select onChange={(e) => setPayload({...payload, branch_id: e.target.value})} id="branch_id" className="bg-gray-50 border border-gray-300 text-gray-900 mb-3 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>What ta'aleem branch do you belong to?</option>
                     <option value=""></option>
                 </select>
                 <div className="relative">
-                    <input type="text" id="" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                    <input type="text" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                     <label for="small_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">If None of the Above, Kindly state your location / area</label>
                 </div>
-                <select id="" className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 mb-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>How did you hear about MDC 2023?</option>
                     <option>Social Media</option>
                     <option>Website</option>
                     <option>Referral</option>
                     <option>Others</option>
                 </select>
-                <label className="block  text-sm font-medium text-gray-900 dark:text-white">Upload your receipt</label>
+                <label className="block text-sm font-medium text-gray-900 dark:text-white">Upload your receipt</label>
                 <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file"  />
 
                 <Button type="submit" id="submit_button">
