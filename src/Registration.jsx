186dc497926bf82db3cb29c28fc1816c15a0ec4b
tarrmmy <tarrmmy@gmail.com>
2024-12-06 09:23:50 +0100

    Correction: Content correction on the registration form reverted(address)

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -26,7 +26,7 @@ function Registration() {
         schoolName : '',
         branchId : '',
         howDidYouHear : '',
-        // address : '',
+        address : '',
         state : '',
     })
 
@@ -229,7 +229,7 @@ function Registration() {
                             </option>
                         ))}
                     </select>
-                    {/* <textarea  onChange={(e) => setPayload({...payload, address: e.target.value})} id='address' rows="1" className="block p-2.5 w-full text-sm mb-1 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="If None of the Above, Kindly state your location / area"></textarea> */}
+                    <textarea  onChange={(e) => setPayload({...payload, address: e.target.value})} id='address' rows="1" className="block p-2.5 w-full text-sm mb-1 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Address/location"></textarea>
                     <select  onChange={(e) => setPayload({...payload, howDidYouHear: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 mb-1 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value=''>How did you hear about MDC 2024?</option>
                         <option>Social Media</option>
