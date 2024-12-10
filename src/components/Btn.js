6850f304a00002f653966003af61b7a0e0cf4a06
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-12-10 13:16:56 +0100

    form reset and modal display

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -9,12 +9,12 @@ const Btn = ({ loading, type, id }) => {
           id={id}
           disabled
           type="button"
-          className="text-white w-full  bg-[#0100f9] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+          className="text-white w-full  bg-[#0100f9] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  flex justify-center items-center"
         >
           <svg
             aria-hidden="true"
             role="status"
-            className="inline items-center text-center w-4 h-4 ml-20 mr-2 text-white animate-spin"
+            className="inline items-center text-center w-4 h-4 mr-2 text-white animate-spin"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +36,7 @@ const Btn = ({ loading, type, id }) => {
           id={id}
           className=" w-full px-5 py-2.5 text-sm font-medium text-white bg-[#0100f9] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Register
         </button>
       )}
     </div>
